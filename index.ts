63b0a56102f77c9e4ab32014fd00bd9635dfa61d
Patrick Smith <patrick.smith@cultureamp.com>
2022-12-02 17:16:43 +1100

    Update to simplified API

--- index.ts
+++ index.ts
@@ -2,20 +2,6 @@ import { serveRequest } from "@collected/press-server";
 
 export default {
   async fetch(request: Request, env: {}, ctx: ExecutionContext): Promise<Response> {
-    return await handleRequest(request).catch(
-      (err) => {
-        if (err instanceof Response) {
-          return err;
-        }
-        console.error(err);
-        return new Response(err.stack, { status: 500 });
-      }
-    );
+    return await serveRequest('RoyalIcing', 'RoyalIcing', new URL(request.url));
   }
-}
-
-async function handleRequest(request: Request): Promise<Response> {
-  const { pathname } = new URL(request.url);
-
-  return serveRequest('RoyalIcing', 'RoyalIcing', pathname);
 }
